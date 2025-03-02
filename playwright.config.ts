094bacb0fb6a7a17e0d9a941d0604e6ee976a3bc
Jeison <jeison.herrera1592@gmail.com>
2025-03-01 20:03:11 -0500

    Login in PriverionPlatform

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
+    headless: false,
+    /* Base URL to use in
+     actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -39,7 +41,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
